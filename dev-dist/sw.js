c9d71af59b8a1ca1029a14c799443536d1cecdcb
TanayaDaundkar <tanayadaundkar@gmail.com>
2025-08-22 14:05:47 +0530

    Initial commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/offline.html",
-    "revision": "0.nv6o0rccs5g"
+    "revision": "0.44h1ffllefo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
